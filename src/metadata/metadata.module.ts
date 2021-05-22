ef349c71185657c67294789a749944fc2f264202
Vectormike <victorjonah199@gmail.com>
2021-05-22 23:38:06 +0100

    ...

--- src/metadata/metadata.module.ts
+++ src/metadata/metadata.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MetadaSchema } from './metadata.model';
 import { MetadataService } from './metadata.service';
 import { MetadataResolver } from './metadata.resolver';
 
 @Module({
-  providers: [MetadataService, MetadataResolver]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Metadata', schema: MetadaSchema }]),
+  ],
+  providers: [MetadataService, MetadataResolver],
 })
 export class MetadataModule {}
